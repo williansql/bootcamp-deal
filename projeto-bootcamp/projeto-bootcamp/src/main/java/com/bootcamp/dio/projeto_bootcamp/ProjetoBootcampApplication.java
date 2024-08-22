35d05ff9e5d28c8d595f64a7000a1b934d593dfb
williansql <willslipknotsp@gmail.com>
2024-08-22 20:22:00 -0300

    first commit

---
+++ projeto-bootcamp/projeto-bootcamp/src/main/java/com/bootcamp/dio/projeto_bootcamp/ProjetoBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dio.projeto_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBootcampApplication.class, args);
+	}
+
+}
---
+++ projeto-bootcamp/projeto-bootcamp/src/test/java/com/bootcamp/dio/projeto_bootcamp/ProjetoBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dio.projeto_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
